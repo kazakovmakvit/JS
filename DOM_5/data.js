d14ec64f31a9c3f98ad74f6427362009491e4365
Max <kazakovmvmvm@gmail.com>
2023-10-02 19:38:50 +0300

    DOM_5 home

---
+++ DOM_5/data.js
@@ -0,0 +1,34 @@
+export const products = `
+[
+   {
+      "imgProduct": "img/mother.jpg",
+      "nameProduct": "Материнские платы",
+      "descriptionProduct": "Материнская плата среднего сегмента на сокете AM5 для нового поколения процессоров семейства AMD",
+      "priceProduct": "400$"
+   },
+   {
+      "imgProduct": "img/CPU.jpg",
+      "nameProduct": "Процессор",
+      "descriptionProduct": "AMD Rusen 7600, 6 ядер, 12 потоков, хватит на игры и на учебу(может быть) ",
+      "priceProduct": "230$"
+   },
+   {
+      "imgProduct": "img/graph.jpg",
+      "nameProduct": "Видеокарта",
+      "descriptionProduct": "RTX 3060 старого образца на 8 гб памяти. Что может быть лучше ?",
+      "priceProduct": "нет в продаже"
+   },
+   {
+      "imgProduct": "img/RAM.jpg",
+      "nameProduct": "ОЗУ",
+      "descriptionProduct": "Оперативка G.Skill на 64 гб 2 плашки + 2 плашки муляж для ощущения полноты картины",
+      "priceProduct": "165$"
+   },
+   {
+      "imgProduct": "img/ssd.png",
+      "nameProduct": "SSD",
+      "descriptionProduct": "SSD накопитель, на 1 Тб хватит с лихвой для хранения всего и вся, если вы не играете в 100500 игр одновременно сохраняя дистрибутивы всех)",
+      "priceProduct": "100$"
+   }
+]
+`
