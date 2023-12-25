63359a20df54ce36b5657dfd5102fe98b8a41573
Max <kazakovmvmvm@gmail.com>
2023-12-25 17:45:26 +0300

    hw-4

--- HW_NPM_4/main.js
+++ HW_NPM_4/main.js
@@ -1,5 +1,3 @@
-// import {getRandomPassword} from "password-generator-fix-lendth";
-
 const {getRandomPassword} = require('password-generator');
 
 console.log(generatePassword(20,true,true,true, true));
