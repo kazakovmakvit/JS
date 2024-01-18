3796fb51739c50c2e3c5e12c585c5581829462e3
Max <kazakovmvmvm@gmail.com>
2024-01-18 18:50:33 +0300

    java_api_1

---
+++ HW_java_api_1/data.js
@@ -0,0 +1,38 @@
+export const workouts = `
+[
+    {
+        "id": 1,
+        "name_of_workout": "Йога",
+        "time_of_workout": "10:00 - 11:00",
+        "max_number_of_participants": 15,
+        "current_number_of_participants": 8
+    },
+    {
+        "id": 2,
+        "name_of_workout": "Пилатес",
+        "time_of_workout": "11:30 - 12:30",
+        "max_number_of_participants": 10,
+        "current_number_of_participants": 5
+    },
+    {
+        "id": 3,
+        "name_of_workout": "Кроссфит",
+        "time_of_workout": "13:00 - 14:00",
+        "max_number_of_participants": 20,
+        "current_number_of_participants": 15
+    },
+    {
+        "id": 4,
+        "name_of_workout": "Танцы",
+        "time_of_workout": "14:30 - 15:30",
+        "max_number_of_participants": 12,
+        "current_number_of_participants": 10
+    },
+    {
+        "id": 5,
+        "name_of_workout": "Бокс",
+        "time_of_workout": "16:00 - 17:00",
+        "max_number_of_participants": 8,
+        "current_number_of_participants": 6
+    }
+]`
